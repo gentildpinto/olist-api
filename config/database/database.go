75f4d34cf575d74dee71732e8ef87cd02f4c94b9
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-10-19 16:01:14 +0100

    removed auto migrator from database.go config file

--- config/database/database.go
+++ config/database/database.go
@@ -1,8 +1,6 @@
 package database
 
 import (
-	"github.com/gentildpinto/olist-api/app/helpers"
-	"github.com/gentildpinto/olist-api/config/logger"
 	"gorm.io/gorm"
 )
 
@@ -10,24 +8,9 @@ var db *gorm.DB
 
 func Initialize(database *gorm.DB) (err error) {
 	db = database
-
-	if helpers.ViperEnvVariable("TRUNCATE_TABLES") == "true" && helpers.ViperEnvVariable("ENVIRONMENT") == "development" {
-		if err = logger.Log(truncateTables(db)); err != nil {
-			return
-		}
-	}
-
-	if err = logger.Log(autoMigrateTables(db)); err != nil {
-		return
-	}
-
 	return
 }
 
-func truncateTables(db *gorm.DB) (err error) {
-	return
-}
-
-func autoMigrateTables(db *gorm.DB) (err error) {
-	return
+func GetDB() *gorm.DB {
+	return db
 }
