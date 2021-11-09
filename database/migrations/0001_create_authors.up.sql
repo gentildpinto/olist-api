fa11d4178f0c72d69a1d12e245646836d2d00ccf
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-11-09 16:05:23 +0100

    updated authors migration

--- database/migrations/0001_create_authors.up.sql
+++ database/migrations/0001_create_authors.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS authors(
-    id              SERIAL PRIMARY KEY,
+    id              BINARY (16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
     name            VARCHAR (50) UNIQUE NOT NULL,
     created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP NULL,
     deleted_at      TIMESTAMP NULL
-);
+) ENGINE=InnoDB;
