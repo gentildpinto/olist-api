e985c661756a7eb8115775fb9b7ba6a2552bd61f
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-11-17 15:35:41 +0100

    added create book function and bind this controller with book service

--- app/controllers/book_controller.go
+++ app/controllers/book_controller.go
@@ -3,22 +3,41 @@ package controllers
 import (
 	"net/http"
 
-	"github.com/gentildpinto/olist-api/app/model"
+	"github.com/gentildpinto/olist-api/app/dto"
+	book "github.com/gentildpinto/olist-api/app/services/book"
 	"github.com/labstack/echo/v4"
 )
 
 var Book = struct {
-	Index func() echo.HandlerFunc
+	Index  func() echo.HandlerFunc
+	Create func() echo.HandlerFunc
 }{
 	Index: func() echo.HandlerFunc {
 		return func(c echo.Context) error {
-			books, err := (model.Book{}).All()
+			books, err := book.All()
 
 			if err != nil {
 				return err
 			}
 
 			return c.JSON(http.StatusOK, books)
+		}
+	},
+	Create: func() echo.HandlerFunc {
+		return func(c echo.Context) error {
+			bookDto := dto.Book{}
+
+			if err := c.Bind(&bookDto); err != nil {
+				return err
+			}
+
+			newBook, err := book.Create(bookDto)
+
+			if err != nil {
+				return err
+			}
+
+			return c.JSON(http.StatusCreated, newBook)
 		}
 	},
 }
