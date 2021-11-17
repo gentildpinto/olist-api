ab0e2a5a40faa93a9117b2fa8a62da84c7b5ba5f
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-11-17 15:34:44 +0100

    model changes

--- app/model/author.go
+++ app/model/author.go
@@ -1,13 +1,24 @@
 package model
 
+import "github.com/gentildpinto/olist-api/config/logger"
+
 type Author struct {
 	Base
-	Name string `json:"name" gorm:"unique"`
+	Name  string `json:"name" gorm:"unique"`
+	Books []Book `json:"books,omitempty" gorm:"many2many:author_books;"`
 }
 
 func (Author) All() (authors []Author, err error) {
-	if err = databaseConnection.Find(&authors).Error; err != nil {
+	if err = logger.Log(databaseConnection.Preload("Books").Find(&authors).Error); err != nil {
 		return []Author{}, err
+	}
+
+	return
+}
+
+func FindAuthorByID(id UUID) (author Author, err error) {
+	if err = logger.Log(databaseConnection.First(&author, id).Error); err != nil {
+		return Author{}, err
 	}
 
 	return
--- app/model/book.go
+++ app/model/book.go
@@ -1,16 +1,26 @@
 package model
 
+import "github.com/gentildpinto/olist-api/config/logger"
+
 type Book struct {
 	Base
-	Name            string `json:"name" gorm:"unique"`
-	Edition         string `json:"edition"`
-	PublicationYear int    `json:"publication_year"`
-	// Authors         []author.Author `json:"authors"`
+	Name            string   `json:"name" gorm:"unique"`
+	Edition         string   `json:"edition"`
+	PublicationYear int      `json:"publication_year"`
+	Authors         []Author `json:"authors,omitempty" gorm:"many2many:author_books;"`
 }
 
 func (Book) All() (books []Book, err error) {
-	if err = databaseConnection.Find(&books).Error; err != nil {
+	if err = logger.Log(databaseConnection.Preload("Authors").Find(&books).Error); err != nil {
 		return []Book{}, err
+	}
+
+	return
+}
+
+func (b *Book) Create() (err error) {
+	if err = logger.Log(databaseConnection.Create(&b).Error); err != nil {
+		return
 	}
 
 	return
