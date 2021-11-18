c6473dadf4f743487c9eff4709886e390473b9d2
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-11-18 15:06:33 +0100

    removed uniqueness of edition field from book table

--- database/migrations/0002_create_books.up.sql
+++ database/migrations/0002_create_books.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS books(
     id               BINARY (16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
     name             VARCHAR (50) UNIQUE NOT NULL,
-    edition          VARCHAR (10) UNIQUE NOT NULL,
+    edition          VARCHAR (10) NOT NULL,
     publication_year INTEGER NOT NULL,
     created_at       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at       TIMESTAMP NULL,
